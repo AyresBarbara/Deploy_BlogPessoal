25ae6bca0cfbfc6e9b81f16f0778101fdb3c9721
Barbara Ayres <ayresbarbara2@gmail.com>
2022-09-05 11:24:46 -0300

    deploy

--- src/main/java/org/generation/blogPessoal/security/BasicSecurityConfig.java
+++ src/main/java/org/generation/blogPessoal/security/BasicSecurityConfig.java
@@ -1,20 +1,39 @@
 package org.generation.blogPessoal.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
-@Configuration
+
 @EnableWebSecurity
-public class BasicSecurityConfig {
+public class BasicSecurityConfig  extends WebSecurityConfigurerAdapter{
+	@Autowired
+	private UserDetailsService userDetailsService;
+
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+	
+		
+		 auth.userDetailsService(userDetailsService);
+
+		 auth.inMemoryAuthentication()
+			.withUser("root")
+			.password(passwordEncoder().encode("root"))
+			.authorities("ROLE_USER");
+
+	}
 
     @Bean
     public PasswordEncoder passwordEncoder() {
